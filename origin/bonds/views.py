f7ca5c76b1b96f710f254681bc1ad1d9254c69fc
DDarby-Lewis <37579938+DDarby-Lewis@users.noreply.github.com>
2020-10-29 18:52:42 +0000

    bonds views

--- origin/bonds/views.py
+++ origin/bonds/views.py
@@ -1,7 +1,27 @@
-from rest_framework.views import APIView
+from rest_framework import generics
 from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+from django_filters import rest_framework as filters
+import django_filters
+
+from .models import Bond
+from .serializers import BondSerializer, UserSerializer
 
 
-class HelloWorld(APIView):
-    def get(self, request):
-        return Response("Hello World!")
+class BondFilter(filters.FilterSet):
+    class Meta:
+        model = Bond
+        fields = ['legal_name', 'isin', 'lei']
+
+class BondList(generics.ListCreateAPIView):
+    queryset = Bond.objects.all()
+    serializer_class = BondSerializer
+    filterset_class = BondFilter
+    def perform_create(self, serializer):
+        serializer.save(user=self.request.user)
+
+class BondDetail(generics.RetrieveUpdateAPIView):
+    queryset = Bond.objects.all()
+    serializer_class = BondSerializer
+    filterset_class = BondFilter
