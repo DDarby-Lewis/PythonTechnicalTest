3024b3e35979f8d30473a774d29acf4859f2487b
DDarby-Lewis <37579938+DDarby-Lewis@users.noreply.github.com>
2020-10-29 18:52:20 +0000

    bonds serializer

---
+++ origin/bonds/serializers.py
@@ -0,0 +1,21 @@
+from rest_framework import serializers
+from django.contrib.auth.models import User
+from .models import Bond
+import requests
+import urllib
+from pprint import pprint
+
+# add serialisers here
+
+class BondSerializer(serializers.ModelSerializer):
+    def create(self, validated_data):
+        pprint(self)
+        if 'legal_name' not in validated_data:
+            url = "https://leilookup.gleif.org/api/v2/leirecords?lei="+validated_data['lei']
+            legal_name = requests.get(url).json()[0]['Entity']['LegalName']['$']
+            validated_data['legal_name'] = urllib.parse.quote_plus(legal_name)
+        return super().create(validated_data)
+
+    class Meta:
+        model = Bond
+        fields = ( 'isin', 'size', 'maturity', 'currency', 'lei', 'legal_name') 
